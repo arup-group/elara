b3b9bb9c043197335b9e921295b8ae20af070f2b
Michael Byrne <16238945+MichaelByrneAU@users.noreply.github.com>
2019-01-16 11:20:12 +1100

    Implement VKT post-processing calculation

--- elara/postprocessing.py
+++ elara/postprocessing.py
@@ -1,6 +1,7 @@
 import os
 
 import geopandas
+import pandas as pd
 
 
 class PostProcessor:
@@ -28,14 +29,48 @@ class VKT(PostProcessor):
         )
 
     def run(self):
-        link_lengths = self.network.link_gdf["length"] / 1000  # Conversion to km
         for mode in self.config.handlers["volume_counts"]:
             file_name = "{}_volume_counts_{}.geojson".format(self.config.name, mode)
             file_path = os.path.join(self.config.output_path, file_name)
-            gdf = geopandas.read_file(file_path)
+            volumes_gdf = geopandas.read_file(file_path)
 
             # Calculate VKT
-            print(file_path)
+            period_headers = generate_period_headers(self.config.time_periods)
+            volumes = volumes_gdf[period_headers]
+            link_lengths = volumes_gdf["length"].values / 1000  # Conversion to metres
+            vkt = volumes.multiply(link_lengths, axis=0)
+            vkt_gdf = pd.concat([volumes_gdf.drop(period_headers, axis=1), vkt], axis=1)
+
+            # Export results
+            csv_path = os.path.join(
+                self.config.output_path, "{}_vkt_{}.csv".format(self.config.name, mode)
+            )
+            geojson_path = os.path.join(
+                self.config.output_path,
+                "{}_vkt_{}.geojson".format(self.config.name, mode),
+            )
+            vkt_gdf.drop("geometry", axis=1).to_csv(csv_path)
+            export_geojson(vkt_gdf, geojson_path)
+
+
+def generate_period_headers(time_periods):
+    """
+    Generate a list of strings corresponding to the time period headers in a typical
+    data output.
+    :param time_periods: Number of time periods across the day
+    :return: List of time period numbers as strings
+    """
+    return list(map(str, range(time_periods)))
+
+
+def export_geojson(gdf, path):
+    """
+    Given a geodataframe, export geojson representation to specified path.
+    :param gdf: Input geodataframe
+    :param path: Output path
+    """
+    with open(path, "w") as file:
+        file.write(gdf.to_json())
 
 
 # Dictionary used to map configuration string to post-processor type
