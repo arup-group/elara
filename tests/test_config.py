58e612eb75cd4ef0721759bee0493c77f5ea5be6
Sean Billings <sean.billings@LDNML07ML414.clients.global.arup.com>
2019-11-27 15:26:11 +0000

    update config override test
    Former-commit-id: bc45a20ab0b92a82feac1eb5ddebb88ab24d955b

--- tests/test_config.py
+++ tests/test_config.py
@@ -2,37 +2,15 @@ from elara.config import Config
 
 def test_config_override_update_input_fields_and_output_path():
     config = Config("tests/test_xml_scenario.toml")
-    events = 'events'
-    network = 'network'
-    path = 'output'
-    overrides = "{" \
-                    "'events': 'events'," \
-                    "'network': 'network'," \
-                    "'path': 'output'" \
-                "}"
+    events = 'output_events.xml.gz'
+    network = 'output_network.xml.gz'
+    override = "/test/path"
 
-    config.override(overrides)
+    config.override(override)
 
-    assert config.parsed_toml['inputs']['events'] == events
-    assert config.parsed_toml['inputs']['network'] == network
+    assert config.parsed_toml['inputs']['events'] == override + "/" + events
+    assert config.parsed_toml['inputs']['network'] == override + "/" + network
 
-    assert config.parsed_toml['outputs']['path'] == path
-    assert config.output_path == path
-
-def test_config_override_ignore_extra_fields():
-    config = Config("tests/test_xml_scenario.toml")
-    events = 'events'
-    network = 'network'
-    overrides = "{" \
-                    "'events': 'events'," \
-                    "'network': 'network'," \
-                    "'extra': 'extra'" \
-                "}"
-
-    config.override(overrides)
-
-    assert config.parsed_toml['inputs']['events'] == events
-    assert config.parsed_toml['inputs']['network'] == network
-
-    assert 'extra' not in config.parsed_toml['inputs']
+    assert config.parsed_toml['outputs']['path'] == override
+    assert config.output_path == override
 
