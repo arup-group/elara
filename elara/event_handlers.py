fixed logic with average speeds

elara/event_handlers.py
@@ -586,7 +586,7 @@ class LinkVehicleSpeeds(EventHandlerTool):
                     duration = end_time - start_time
 
                 self.counts[x, y, z] += 1
-                self.duration_sum[x, y, z] += duration
+                self.duration_sum[x, y, z] += 1/duration
                 self.duration_max[x, y, z] = max(duration, self.duration_max[x, y, z]) 
 
                 if self.duration_min[x, y, z] == 0: #needs this condition or else the minimum duration would ever budge from zero
@@ -607,8 +607,14 @@ class LinkVehicleSpeeds(EventHandlerTool):
         def counts_holder_generator(calc_type):
             unit_matrix = np.ones((len(self.elem_indices), len(self.classes), self.config.time_periods))
             if calc_type == "average":
-                counts_holder = np.divide(self.counts, self.duration_sum, out=np.zeros_like(self.counts), where=self.duration_sum!=0)
-                counts_holder_reduced = counts_holder.sum(1)
+                counts_holder = self.duration_sum
+                counts_holder_reduced = counts_holder
+                counts_holder_reduced[counts_holder_reduced == 0] = np.nan
+                #np.divide(self.counts, self.duration_sum, out=np.zeros_like(self.counts), where=self.duration_sum!=0)
+                #counts_holder_reduced = counts_holder.mean(1)
+                counts_holder_reduced = np.nanmean(counts_holder_reduced, axis = 1)
+                counts_holder_reduced = np.nan_to_num(counts_holder_reduced, nan = 0)
+                print(counts_holder_reduced)
             elif calc_type == "min":
                 counts_holder = np.divide(unit_matrix, self.duration_max, out=np.zeros_like(unit_matrix), where=self.duration_max!=0)
                 counts_holder_reduced = counts_holder

tests/test_2_event_handlers.py
@@ -434,12 +434,12 @@ def test_link_speed_process_events_car(test_car_link_speed_handler, car_link_pai
     for elem in car_link_pair_event:
         handler.process_event(elem)
     assert np.sum(handler.counts) == 2
-    assert np.sum(handler.duration_sum)== 250
+    assert np.sum(handler.duration_sum)== 0.025
     link_index = handler.elem_indices['1-2']
     class_index = handler.class_indices['rich']
     period = 6
     assert handler.counts[link_index][class_index][period] == 1
-    assert handler.duration_sum[link_index][class_index][period] == 200
+    assert handler.duration_sum[link_index][class_index][period] == 1/200
 
 
 def test_link_speed_finalise_car(test_car_link_speed_handler, car_link_pair_event):
@@ -453,7 +453,7 @@ def test_link_speed_finalise_car(test_car_link_speed_handler, car_link_pair_even
             assert c in gdf.columns
         df = gdf.loc[:, cols]
         if name == "link_vehicle_speeds_car_average":
-            assert np.sum(df.values) == 2.5
+            assert np.sum(df.values) == 1.25
         elif name == "link_vehicle_speeds_car_min":
             assert np.sum(df.values) == 0.5
         elif name == "link_vehicle_speeds_car_max":