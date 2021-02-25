073b92491861fe8f92d22ba86318dec1786f1a13
KasiaKoz <kasia.kozlowska@arup.com>
2021-02-25 18:40:42 +0000

    add method to decode encoded polyline

--- elara/helpers.py
+++ elara/helpers.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import click
-
+import polyline
+from shapely.geometry import LineString
 
 class PathPath(click.Path):
     """A Click path argument that returns a pathlib Path, not a string"""
@@ -24,3 +25,12 @@ def camel_to_snake(text):
         else:
             out = out + t
     return out
+
+
+def decode_polyline_to_shapely_linestring(_polyline):
+    """
+    :param _polyline: google encoded polyline
+    :return: shapely.geometry.LineString
+    """
+    decoded = polyline.decode(_polyline)
+    return LineString(decoded)
