847f2d944c220e799d5ba3aa8b793046799cde20
fred.shone <fred.shone@arup.com>
2021-06-02 10:48:57 +0100

    overrides road pricing fix

--- tests/test_0_config.py
+++ tests/test_0_config.py
@@ -7,12 +7,14 @@ def test_config_override_update_input_fields_and_output_path():
     config = Config("tests/test_xml_scenario.toml")
     events = 'output_events.xml.gz'
     network = 'output_network.xml.gz'
+    road_pricing = './tests/test_fixtures/road_pricing.xml'
     override = "/test/path"
 
     config.override(override)
 
     assert config.settings['inputs']['events'] == override + "/" + events
     assert config.settings['inputs']['network'] == override + "/" + network
+    assert config.settings['inputs']['road_pricing'] == road_pricing
 
     assert config.settings['outputs']['path'] == override + "/" + config.settings['outputs']['path'].split("/")[-1]
     assert config.output_path == override + "/" + config.settings['outputs']['path'].split("/")[-1]
