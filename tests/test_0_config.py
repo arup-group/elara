ad18e1df9ef99365ecde704ad428ab646abfecf5
fred.shone <fred.shone@arup.com>
2021-06-02 14:05:20 +0100

    cnfg override json dump

--- tests/test_0_config.py
+++ tests/test_0_config.py
@@ -1,4 +1,6 @@
 import pytest
+import os
+import json
 
 from elara.config import Config
 
@@ -10,14 +12,25 @@ def test_config_override_update_input_fields_and_output_path():
     road_pricing = './tests/test_fixtures/road_pricing.xml'
     override = "/test/path"
 
-    config.override(override)
+    config.override(override, dump_log=False)
 
-    assert config.settings['inputs']['events'] == override + "/" + events
-    assert config.settings['inputs']['network'] == override + "/" + network
+    assert config.settings['inputs']['events'] == os.path.join(override, events)
+    assert config.settings['inputs']['network'] == os.path.join(override, network)
     assert config.settings['inputs']['road_pricing'] == road_pricing
 
-    assert config.settings['outputs']['path'] == override + "/" + config.settings['outputs']['path'].split("/")[-1]
-    assert config.output_path == override + "/" + config.settings['outputs']['path'].split("/")[-1]
+    assert config.settings['outputs']['path'] == os.path.join(override, config.settings['outputs']['path'].split("/")[-1])
+    assert config.output_path == os.path.join(override, config.settings['outputs']['path'].split("/")[-1])
+
+
+def test_dump_overrides_to_json(tmpdir):
+    config = Config("tests/test_xml_scenario.toml")
+    assert os.path.exists(tmpdir)
+    path = os.path.join(tmpdir, "cnfg.json")
+    config.dump_settings_to_disk(path)
+    assert os.path.exists(path)
+    with open(path) as file:
+        cnfg = json.load(file)
+    assert cnfg["inputs"]["events"] == "./tests/test_fixtures/output_events.xml.gz"
 
 
 def test_version_11_attributes_path_is_attributes():
