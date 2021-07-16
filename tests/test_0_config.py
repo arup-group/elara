994a5d1ede165c6c6048d4f27f0629fec7454eb1
fred.shone <fred.shone@arup.com>
2021-07-16 21:48:20 +0100

    adds test

--- tests/test_0_config.py
+++ tests/test_0_config.py
@@ -17,9 +17,27 @@ def test_config_override_update_input_fields_and_output_path():
     assert config.settings['inputs']['events'] == os.path.join(override, events)
     assert config.settings['inputs']['network'] == os.path.join(override, network)
     assert config.settings['inputs']['road_pricing'] == road_pricing
-
     assert config.settings['outputs']['path'] == os.path.join(override, config.settings['outputs']['path'].split("/")[-1])
     assert config.output_path == os.path.join(override, config.settings['outputs']['path'].split("/")[-1])
+
+
+def test_config_set_paths_roots_to_input_fields_and_output_path_and_bm_data_path():
+    config = Config("tests/test_xml_scenario_dictionary.toml")
+    events = './tests/test_fixtures/output_events.xml.gz'
+    network = './tests/test_fixtures/output_network.xml.gz'
+    road_pricing = './tests/test_fixtures/road_pricing.xml'
+    out_path = './tests/test_intermediate_data'
+    bm_data_path = './tests/test_outputs/trip_duration_breakdown_all.csv'
+    root = "/test/path"
+
+    config.set_paths_root(root, dump_log=False)
+
+    assert config.settings['inputs']['events'] == os.path.join(root, events)
+    assert config.settings['inputs']['network'] == os.path.join(root, network)
+    assert config.settings['inputs']['road_pricing'] == os.path.join(root, road_pricing)
+    assert config.settings['outputs']['path'] == os.path.join(root, out_path)
+    assert config.output_path == os.path.join(root, out_path)
+    assert config.settings['benchmarks']['duration_comparison']['benchmark_data_path'] == os.path.join(root, bm_data_path)
 
 
 def test_dump_overrides_to_json(tmpdir):
