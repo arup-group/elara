fix speed averaging (#185)
* fix speed averaging

* fix test

* refactoring

elara/event_handlers.py
@@ -567,7 +567,7 @@ class LinkVehicleSpeeds(EventHandlerTool):
                                 self.config.time_periods))
 
         # Initialise duration cummulative sum table
-        self.inverseduration_sum = np.zeros((len(self.elem_indices), len(self.classes), self.config.time_periods))
+        self.duration_sum = np.zeros((len(self.elem_indices), len(self.classes), self.config.time_periods))
         self.duration_min = np.zeros((len(self.elem_indices), len(self.classes), self.config.time_periods))
         self.duration_max = np.zeros((len(self.elem_indices), len(self.classes), self.config.time_periods))
 
@@ -648,7 +648,7 @@ class LinkVehicleSpeeds(EventHandlerTool):
                     self.counts[x, y, z] += 1
 
                     if duration != 0:
-                        self.inverseduration_sum[x, y, z] += 1/duration
+                        self.duration_sum[x, y, z] += duration
 
                     self.duration_max[x, y, z] = max(duration, self.duration_max[x, y, z])
 
@@ -667,11 +667,20 @@ class LinkVehicleSpeeds(EventHandlerTool):
 
         mps_to_kph = 3.6
 
-        def calc_av_matrices(self):
-            counts_pop = self.counts.sum(1)
-            duration_pop = self.inverseduration_sum.sum(1)
-            av_pop = np.divide(duration_pop, counts_pop, out=np.zeros_like(counts_pop), where=duration_pop != 0)
-            return av_pop
+        def calc_average_speed(self, counts_link:pd.DataFrame, duration_link:pd.DataFrame)->pd.DataFrame:
+            """
+            Calculate link average speed. 
+            Average speed = (total distance travelled) / (total travel duration) = 
+                            (n * link_distance) / sum(travel duration)
+            """
+            # number of vehicles divided by the total travel time on the link (n/Σ(t_i))
+            average_speeds = np.divide(counts_link, duration_link, out=np.zeros_like(duration_link), where=duration_link != 0).fillna(0)
+
+            # multiply by link distances to derive average speed
+            average_speeds = self.elem_gdf.join(average_speeds.reset_index().set_index('elem'), how="left")
+            average_speeds = multiply_distance(self, average_speeds)
+
+            return average_speeds
 
         def calc_max_matrices(self):
             unit_matrix = np.ones((len(self.elem_indices), len(self.classes), self.config.time_periods))
@@ -699,39 +708,46 @@ class LinkVehicleSpeeds(EventHandlerTool):
             index = pd.MultiIndex.from_product(indexes, names=names)
             df = pd.DataFrame(subpop_matrix.flatten(), index=index)[0]
             df = df.unstack(level='hour').sort_index()
-            df = df.reset_index().set_index('elem')
             return df
 
-        def calc_speeds(self, df):  # converts 1/duration matrix into speeds by multiplying through by length
+        def multiply_distance(self, df): 
+            """
+            Multiplies time period columns (ie hour 0...23) by link length
+            """
             for i in range(self.config.time_periods):
                 df[i] = df[i] * df["length"]
             return df
 
         if self.groupby_person_attribute:
-            # Calc average at subpop level
+            # Calc average speed at subpopulation level
             key = f"{self.name}_average_{self.groupby_person_attribute}"
-            average_speeds = flatten_subpops(self, self.inverseduration_sum)
-            average_speeds = self.elem_gdf.join(average_speeds, how="left")
-            average_speeds = calc_speeds(self, average_speeds)
+            counts_link_subpop = flatten_subpops(self, self.counts)   
+            duration_link_subpop = flatten_subpops(self, self.duration_sum)    
+            average_speeds = calc_average_speed(self, counts_link_subpop, duration_link_subpop)
+
             average_speeds.index.name = "id"
             self.result_dfs[key] = average_speeds
 
-        # Calc average at pop level
+        # Calc average speed at population level
         key = f"{self.name}_average"
-        average_speeds = calc_av_matrices(self)
-        average_speeds = pd.DataFrame(
-                data=average_speeds, index=self.elem_ids, columns=range(0, self.config.time_periods)
-            ).sort_index()
-        average_speeds = self.elem_gdf.join(average_speeds, how="left")
-        average_speeds = calc_speeds(self, average_speeds)
+        # total link counts by hour
+        counts_link_pop = pd.DataFrame(
+            data=self.counts.sum(1), index=self.elem_ids, columns=range(0, self.config.time_periods)
+            ).sort_index().rename_axis('elem')
+        # sum of link travel duration by hour 
+        duration_link_pop = pd.DataFrame(
+            data=self.duration_sum.sum(1), index=self.elem_ids, columns=range(0, self.config.time_periods)
+            ).sort_index().rename_axis('elem')
+
+        average_speeds = calc_average_speed(self, counts_link_pop, duration_link_pop)
         self.result_dfs[key] = average_speeds
 
         if self.groupby_person_attribute:
             # Calc max at subpop level
             key = f"{self.name}_max_{self.groupby_person_attribute}"
-            max_speeds = flatten_subpops(self, calc_max_matrices(self)[0])
+            max_speeds = flatten_subpops(self, calc_max_matrices(self)[0]).reset_index().set_index('elem')
             max_speeds = self.elem_gdf.join(max_speeds, how="left")
-            max_speeds = calc_speeds(self, max_speeds)
+            max_speeds = multiply_distance(self, max_speeds)
             max_speeds.index.name = "id"
             self.result_dfs[key] = max_speeds
 
@@ -742,16 +758,16 @@ class LinkVehicleSpeeds(EventHandlerTool):
                 data=max_speeds, index=self.elem_ids, columns=range(0, self.config.time_periods)
             ).sort_index()
         max_speeds = self.elem_gdf.join(max_speeds, how="left")
-        max_speeds = calc_speeds(self, max_speeds)
+        max_speeds = multiply_distance(self, max_speeds)
         self.result_dfs[key] = max_speeds
 
         if self.groupby_person_attribute:
             # Calc min at subpop level
             key = f"{self.name}_min_{self.groupby_person_attribute}"
             min_matrix = calc_min_matrices(self)[0]
-            min_speeds = flatten_subpops(self, min_matrix)
+            min_speeds = flatten_subpops(self, min_matrix).reset_index().set_index('elem')
             min_speeds = self.elem_gdf.join(min_speeds, how="left")
-            min_speeds = calc_speeds(self, min_speeds)
+            min_speeds = multiply_distance(self, min_speeds)
             min_speeds.index.name = "id"
             self.result_dfs[key] = min_speeds
 
@@ -762,7 +778,7 @@ class LinkVehicleSpeeds(EventHandlerTool):
                 data=min_speeds, index=self.elem_ids, columns=range(0, self.config.time_periods)
             ).sort_index()
         min_speeds = self.elem_gdf.join(min_speeds, how="left")
-        min_speeds = calc_speeds(self, min_speeds)
+        min_speeds = multiply_distance(self, min_speeds)
         self.result_dfs[key] = min_speeds
 
         # convert all dataframes from meters per second to kph

tests/test_2_event_handlers.py
@@ -480,12 +480,12 @@ def test_link_speed_process_events_car(test_car_link_speed_handler, car_link_pai
     for elem in car_link_pair_event:
         handler.process_event(elem)
     assert np.sum(handler.counts) == 3
-    assert np.sum(handler.inverseduration_sum)== 0.11
+    assert np.sum(handler.duration_sum)== 95
     link_index = handler.elem_indices['1-2']
     class_index = handler.class_indices['poor']
     period = 6
     assert handler.counts[link_index][class_index][period] == 2
-    assert handler.inverseduration_sum[link_index][class_index][period] == 1/50+1/25
+    assert handler.duration_sum[link_index][class_index][period] == 50+25
 
 
 def test_link_speed_finalise_car(test_car_link_speed_handler, car_link_pair_event):
@@ -500,9 +500,9 @@ def test_link_speed_finalise_car(test_car_link_speed_handler, car_link_pair_even
             assert c in gdf.columns
         df = gdf.loc[:, cols]
         if name == "link_vehicle_speeds_car_average":
-            assert np.sum(df.values) == (2+5+4)/3 * 3.6
+            assert np.sum(df.values) == 3 / (1/2 + 1/5 + 1/4) * 3.6
         elif name == "link_vehicle_speeds_car_average_subpopulation":
-            assert np.sum(df.values) == (2+5+4) * 3.6
+            assert np.sum(df.values) == 18 + 9.6
         elif name == "link_vehicle_speeds_car_min":
             assert np.sum(df.values) == 2 * 3.6
         elif name == "link_vehicle_speeds_car_min_subpopulation": # TODO something wrong here