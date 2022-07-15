1b5c9a3ac4b17d5ad08849a4bf9394ebc1c1c67b
fred.shone <fred.shone@arup.com>
2022-07-15 18:05:51 +0100

    improves csv comparison plotting

--- tests/test_10_helpers.py
+++ tests/test_10_helpers.py
@@ -1,7 +1,7 @@
 import pytest
 from shapely.geometry import LineString
 
-from elara.helpers import camel_to_snake, decode_polyline_to_shapely_linestring
+from elara.helpers import camel_to_snake, decode_polyline_to_shapely_linestring, longest_numeric
 
 
 test_text_data = [
@@ -19,3 +19,14 @@ def test_camel_to_snake(textin, textout):
 def test_decode_polyline_to_linestring():
     ls = decode_polyline_to_shapely_linestring('emmkhtaBwfemhs`@qxhmKheziWpxhmKieziW')
     assert ls == LineString([(529705.93507, 180572.3558), (529771.19356, 180444.74903), (529705.93507, 180572.3558)])
+
+
+test_string_data = [
+    ("123", 123),
+    ("12M", 12),
+    ("AAA1", []),
+    ("", []),
+]
+@pytest.mark.parametrize("string,number", test_string_data)
+def test_longest_numeric(string, number):
+    assert longest_numeric(string) == number
